123dec64a5333bd82e52cc537466bda7600b3138
Glitch (parser-header) <none>
2018-05-13 00:59:00 +0000

    💫⛵️ Checkpoint ./server.js:1047756/96 ./views/index.html:1047756/627

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
+  console.log(request);
+
 });
+
+
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
